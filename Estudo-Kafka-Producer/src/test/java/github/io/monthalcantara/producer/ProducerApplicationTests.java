756e47b5fdb6e0548250d1510a7fb56ad943957b
monthalcantara <montival_junior@yahoo.com.br>
2021-05-04 21:40:53 -0300

    Adicionado projeto de producer

---
+++ Estudo-Kafka-Producer/src/test/java/github/io/monthalcantara/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package github.io.monthalcantara.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
