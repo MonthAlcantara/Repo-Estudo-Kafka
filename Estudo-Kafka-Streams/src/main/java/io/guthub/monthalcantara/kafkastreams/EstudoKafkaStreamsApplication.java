ceb5dad0a14042c53e3c181d0c85a1ab5361ccbd
MonthAlcantara <montival_junior@yahoo.com.br>
2024-11-16 14:56:32 -0300

    Adicionado novo projeto de estudo

---
+++ Estudo-Kafka-Streams/src/main/java/io/guthub/monthalcantara/kafkastreams/EstudoKafkaStreamsApplication.java
@@ -0,0 +1,13 @@
+package io.guthub.monthalcantara.kafkastreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoKafkaStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoKafkaStreamsApplication.class, args);
+	}
+
+}
---
+++ Estudo-Kafka-Streams/src/test/java/io/guthub/monthalcantara/kafkastreams/EstudoKafkaStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.guthub.monthalcantara.kafkastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoKafkaStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
